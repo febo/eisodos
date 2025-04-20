7650b2964e5f941723ed6bb634584386a7c5ddb0
Han Yang <dhy1996@live.com.sg>
2025-04-21 07:13:12 +0800

    upgrade to solana 2.2.6 (#1)

--- scripts/program/build.mjs
+++ scripts/program/build.mjs
@@ -1,17 +1,12 @@
 #!/usr/bin/env zx
 import 'zx/globals';
 import {
-  cliArguments,
   getProgramFolders,
   workingDirectory,
 } from '../utils.mjs';
 
 // Save external programs binaries to the output directory.
 import './dump.mjs';
-
-// Configure additional arguments here, e.g.:
-// ['--arg1', '--arg2', ...cliArguments()]
-const buildArgs = cliArguments();
 
 // Build the programs.
 for (const folder of getProgramFolders()) {
